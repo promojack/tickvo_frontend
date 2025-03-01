5d2e86f7d80b6347c8a1dd8a9b8d62290e5a2bfd
johnkratos1 <113154336+johnkratos1@users.noreply.github.com>
2025-03-01 17:21:37 +0100

    run small update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
-    host: "192.168.170.28", // Replace with your actual IP
+    host: "192.168.250.28", // Replace with your actual IP192.168.250.28
     port: 5173, // Change if needed
     strictPort: true, // Ensures the exact port is used
   },
